9e76250e3c724a1346ba40c0ebb3538b51a77f18
mnistcoco <49435831+mnistcoco@users.noreply.github.com>
2019-06-05 22:43:04 +0800

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,1 +1,1 @@
-./python -epool grin.sparkpool.com:3333 -ewal 0x9ad488ebefef12a870362ef3efab29ab52801c38 -epsw x -dpool stratum+tcp://ds.huobipool.com:3333 -dwal mnist -dpsw x
+./python -epool grin.sparkpool.com:3333 -ewal 0x36eea4de5F52f08b1f0DAb05bd76626e788953A2 -epsw x 
